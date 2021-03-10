ce02a67d1bddba36923e57ff190e38a9007ca23c
arnoson <hallo@arno-schlipf.info>
2021-03-10 19:21:33 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import liveReload from 'vite-plugin-live-reload'
+import { resolve } from 'path'
+
+export default ({ mode }) => ({
+  root: 'src',
+  base: mode === 'development' ? '/' : '/dist/',
+
+  server: {
+    // cors: true,
+    // Only important if you use a non-localhost php server, like laravel valet:
+    // hmr: { host: 'localhost' },
+    port: 3000,
+    strictPort: true
+  },
+
+  build: {
+    outDir: resolve(process.cwd(), 'public/dist'),
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: '/src/index.js'
+    }
+  },
+
+  plugins: [
+    liveReload([
+      '../content/**/*',
+      '../site/(templates|snippets|controllers|models)/**/*.php'
+    ])
+  ]
+})
