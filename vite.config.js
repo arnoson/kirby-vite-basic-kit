69ab82e139bbf624d598e8f6348d9cb22694cd9e
arnoson <omasocke@hotmail.de>
2022-09-20 14:37:09 +0200

    chore: update `kirby-vite`

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,15 @@
-import liveReload from 'vite-plugin-live-reload'
 import { resolve } from 'path'
+import kirby from 'vite-plugin-kirby'
 
 export default ({ mode }) => ({
   root: 'src',
   base: mode === 'development' ? '/' : '/dist/',
 
-  server: {
-    // Only important if you use a non-localhost php server, like laravel valet:    
-    // cors: true,
-    // hmr: { host: 'localhost' },
-    origin: 'http://localhost:3000',
-    port: 3000,
-    strictPort: true
-  },
-
   build: {
     outDir: resolve(process.cwd(), 'public/dist'),
     emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: resolve(process.cwd(), 'src/index.js')
-    }
+    rollupOptions: { input: resolve(process.cwd(), 'src/index.js') }
   },
 
-  plugins: [
-    liveReload(['content/**/*', 'site/**/*.php'], { root: process.cwd() })
-  ]
+  plugins: [kirby()]
 })
