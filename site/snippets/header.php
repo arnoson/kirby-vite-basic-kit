cbdabc3793909ac55c67d46627ba00c7931379b8
arnoson <hallo@arno-schlipf.info>
2023-08-13 14:21:42 +0200

    feat!: use `kirby-vite` 4

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,6 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Kirby Vite Basic</title>
-  <?= vite()->css() ?>
+  <?= vite()->js('index.js', ['defer' => true]) ?>
+  <!-- We imported `index.css` inside `index.js`, so `index.js` is the Vite
+  entry containing our CSS. -->
+  <?= vite()->css('index.js') ?>
 </head>
 <body>
