9756f60d08e8a399fded467910410e590b2fa4b9
arnoson <hallo@arno-schlipf.info>
2023-11-28 18:37:16 +0100

    chore: update dependencies

--- site/config/vite.config.php
+++ site/config/vite.config.php
@@ -1,4 +1,7 @@
-<?php return [
+<?php
+// This is an auto-generated file. Please avoid making changes here.
+// Configure your settings in the "vite.config.js" file instead.
+return [
   'outDir' => 'public/dist',
   'assetsDir' => 'assets',
   'legacy' => false
